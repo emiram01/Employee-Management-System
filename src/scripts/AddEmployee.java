2cf799ebcefb37748719574f115ebbb9292c64d1
DemetriQG <91648310+DemetriQG@users.noreply.github.com>
2021-11-23 22:06:40 -0800

    Update AddEmployee.java
    Implemented the add employee feature.

--- src/scripts/AddEmployee.java
+++ src/scripts/AddEmployee.java
@@ -1,14 +1,27 @@
 package scripts;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import java.io.IOException;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class AddEmployee
 {
     @FXML private Button backButton;
     @FXML private Button logoutButton;
+    @FXML private TextField employeeID;
+    @FXML private TextField employeeName;
+    @FXML private TextField employeeSalary;
+    @FXML private TextField employeePhoneNum;
+    @FXML private TextField employeeAddress;
+    @FXML private Button submitButton;
+    @FXML private Label addEmpText;
 
     public void goBack(ActionEvent event) throws IOException
     {
@@ -20,5 +33,51 @@ public class AddEmployee
     {
         App app = new App();
         app.changeScene("login.fxml");
+    }
+
+    public void checkSubmit(ActionEvent event) throws IOException
+    {
+        validate();
+    }
+
+    private void validate() throws IOException
+    {
+        if(employeeID.getText().equals("") || employeeName.getText().equals("") || employeeSalary.getText().equals("") ||employeeAddress.getText().equals("")||employeePhoneNum.getText().equals(""))
+        {
+            addEmpText.setText("Missing info, please try agian.");  
+        }
+        else
+        {
+            String name = employeeName.getText();
+            String id = employeeID.getText();
+            String salary = employeeSalary.getText();
+            String address = employeeAddress.getText();
+            String phone = employeePhoneNum.getText();
+            employeeID.setText("");
+            employeeName.setText("");
+            employeeSalary.setText("");
+            employeeAddress.setText("");
+            employeePhoneNum.setText("");
+
+                try(FileWriter fw = new FileWriter("src/data/EmployeeData.txt", true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw))
+                {
+                    fw.write("\n");
+                    fw.write(id);
+                    fw.write(" ");
+                    fw.write(name);
+                    fw.write(" ");
+                    fw.write(address);
+                    fw.write(" ");
+                    fw.write(phone);
+                    fw.write(" ");
+                    fw.write(salary);
+                    fw.close();
+                    addEmpText.setText("Employee Profile Saved");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 }
